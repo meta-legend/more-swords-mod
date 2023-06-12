4ceca40d557ddcf3064cf51f6c19e41eeccbcbb2
meta-legend <110937181+meta-legend@users.noreply.github.com>
2023-06-12 00:07:19 -0500

    Make the netherite katana harder to make & added post-hit effects whenever you hit someone with the netherite katana

--- src/main/java/net/metalegend/moreswordsmod/item/Katana.java
+++ src/main/java/net/metalegend/moreswordsmod/item/Katana.java
@@ -1,11 +1,25 @@
 package net.metalegend.moreswordsmod.item;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.item.ToolMaterials;
 
 public class Katana extends SwordItem {
     public Katana(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, FabricItemSettings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
+    }
+
+    @Override
+    public boolean postHit(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        if (getMaterial() == ToolMaterials.NETHERITE) {
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.WEAKNESS, 100, 1));
+            target.addStatusEffect(new StatusEffectInstance(StatusEffects.HUNGER, 50, 1));
+        }
+        return super.postHit(stack, target, attacker);
     }
 }
